1ad12d90e9457f7e268568ed9306429449024cb5
Ichicka1 <120476628+Ichicka1@users.noreply.github.com>
2024-10-30 18:26:28 +0100

    Update loader.js

--- loader.js
+++ loader.js
@@ -1,5 +1,5 @@
 const loader = new THREE.GLTFLoader();
-loader.load('path/to/your_model.glb', function(gltf) {
+loader.load('scene.gltf', function(gltf) {
     scene.add(gltf.scene);
 }, undefined, function(error) {
     console.error(error);
